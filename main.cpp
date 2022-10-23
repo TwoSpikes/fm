063e094257994cfb241168e9ebd82c414ce4b4b0
TwoSpikes <twospikesfpg@gmail.com>
2022-10-24 00:13:17 +0300

    No description

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main(int argc, char **argv) {
-  for(int i = 0; i < argc; ++i) {
-    std::cout << argv[i] << std::endl;
+  auto &args = *new std::vector<std::string>;
+  for(int i = 1; i < argc; ++i) {
+    args.push_back(argv[i]);
+  }
+  if(args.size()) {
+    if(!args[0].compare("ls")) {
+      for(int i = 1; i < args.size(); ++i) {
+	std::cout << args[i] << std::endl;
+      }
+      return 0;
+    }
   }
   return 0;
 }
