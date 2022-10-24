a420557d3c476101ae563392cd0f06a0efb02d33
TwoSpikes <twospikesfpg@gmail.com>
2022-10-24 16:12:08 +0300

    Ready? Set. Go!

--- main.cpp
+++ main.cpp
@@ -1,18 +1,47 @@
+#include <filesystem>
+namespace fs = std::__fs::filesystem;
+#include <functional>
 #include <iostream>
 #include <vector>
 #include <string>
+
+bool stdCondition(const fs::path &x) {
+  return x.c_str()[2] != '.';
+}
+
+bool allCondition(const fs::path &x) {
+  return true;
+}
+
+bool AllCondition(const fs::path &x) {
+  return !strcmp(x.c_str(), "./..") &&
+    !strcmp(x.c_str(), "./.");
+}
 
 int main(int argc, char **argv) {
   auto &args = *new std::vector<std::string>;
   for(int i = 1; i < argc; ++i) {
     args.push_back(argv[i]);
   }
-  if(args.size()) {
-    if(!args[0].compare("ls")) {
-      for(int i = 1; i < args.size(); ++i) {
-	std::cout << args[i] << std::endl;
+  bool(*condition)(const fs::path &x) = &stdCondition;
+  for(int i = 0; i < args.size(); ++i) {
+    auto &src = *new std::string(args[i]);
+    if(!src.compare("-a") ||
+       !src.compare("--all")) {
+      condition = &allCondition;
+      continue;
+    }
+    for(const auto &entry: fs::directory_iterator(src)) {
+      auto &p = entry.path();
+      if(!condition(p)) {
+	continue;
       }
-      return 0;
+      if(fs::is_directory(p)) {
+	std::cout << "\033[1;32m";
+      } else {
+	std::cout << "\033[1;37m";
+      }
+      std::cout << *new std::string(p.c_str()) << std::endl;
     }
   }
   return 0;
