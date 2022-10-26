ad6e9e0b1ee7778084b40d230d3bbc919e596c1c
TwoSpikes <twospikesfpg@gmail.com>
2022-10-26 21:13:34 +0300

    Fix bug

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ namespace fs = std::__fs::filesystem;
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <deque>
 #include <map>
 
 typedef u_int8_t u8;
@@ -14,13 +15,33 @@ enum Condition {
   DIR = 0b10,
 };
 
-bool CheckCondition(u8 condition, const fs::path &x) {
+bool CheckCondition(u8 condition,
+		    const fs::path &x,
+		    size_t lenCurDir = 0) {
   if(!condition) return true;
-  if(condition & STD && x.c_str()[2] == '.' )
+  if(condition & STD && x.c_str()[lenCurDir] == '.' )
     return false;
   if(condition & DIR && fs::is_directory(x) )
     return false;
   return true;
+}
+
+void HandlePath(const fs::path &path,
+		u8 condition = 0b0,
+		size_t lenCurDir = 0) {
+  auto &pathStr = *new std::string(path.c_str());
+  pathStr = pathStr.substr(lenCurDir);
+  if(!CheckCondition(condition, path, lenCurDir)) {
+    return;
+  }
+  if(fs::is_directory(path)) {
+    std::cout << "\033[36m";
+  } else if(pathStr.back() == '~') {
+    std::cout << "\033[37m";
+  } else {
+    std::cout << "\033[1m\033[37m";
+  }
+  std::cout << pathStr << std::endl << "\033[0m";
 }
 
 int main(int argc, char **argv) {
@@ -28,8 +49,8 @@ int main(int argc, char **argv) {
   for(int i = 1; i < argc; ++i) {
     args.push_back(argv[i]);
   }
-  u8 condition = STD;
-  bool d_option = false;
+  u8 condition  = STD;
+  bool reversed = false;
   for(int i = 0; i < args.size(); ++i) {
     auto &src = *new std::string(args[i]);
     if(!src.compare("-a") ||
@@ -43,28 +64,25 @@ int main(int argc, char **argv) {
       condition |= DIR;
       continue;
     }
+    if(!src.compare("-r") ||
+       !src.compare("--reverse")) {
+      reversed = true;
+      continue;
+    }
     if(src[0] == '-') {
       std::cout << "Unknown option: " << src << std::endl;
       continue;
     }
-    for(const auto &entry: fs::directory_iterator(src)) {
-      auto &p = entry.path();
-      auto &ps = *new std::string(p.c_str());
-      if(!CheckCondition(condition, p)) {
-	continue;
-      }
-      if(fs::is_directory(p)) {
-	std::cout << "\033[36m";
-      } else if(ps.back() == '~') {
-	std::cout << "\033[37m";
-      } else {
-	std::cout << "\033[1m\033[37m";
-      }
-      std::cout <<
-	*new std::string(p.c_str()) <<
-	std::endl <<
-	"\033[0m";
-    }
+
+    const size_t lenCurDir = src.length() - (src.back() == '/');
+    if(!reversed)
+      for(const auto &entry: fs::directory_iterator(src))
+	HandlePath(entry.path(), condition, lenCurDir);
+    auto &res = *new std::deque<const fs::path>;
+    for(const auto &entry: fs::directory_iterator(src))
+      res.push_front(entry.path());
+    for(auto it = res.rbegin(); it != res.rbegin(); --it)
+      HandlePath(*it, condition, lenCurDir);
   }
   return 0;
 }
