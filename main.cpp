ecc3178bf40768e8be42d62de66d8d2b251345f7
TwoSpikes <twospikesfpg@gmail.com>
2022-11-11 21:54:41 +0300

    Fixbug

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <filesystem>
 namespace fs = std::__fs::filesystem;
 #include <functional>
+#include <algorithm>
 #include <iostream>
 #include <cstdlib>
 #include <vector>
@@ -22,32 +23,31 @@ enum Condition {
   STD = 0b1,
   DIR = 0b10,
 };
+Condition const DEFAULT_CONDITION = STD;
 
+//checks, print file or not
 bool CheckCondition(u8 condition,
-		    const fs::path &x,
-		    size_t lenCurDir = 0) {
+		    const fs::path &path) {
   if(!condition) return true;
-  if(condition & STD && x.c_str()[lenCurDir] == '.' )
+  if(condition & STD && path.u8string()[0] == '.' )
     return false;
-  if(condition & DIR && fs::is_directory(x) )
+  if(condition & DIR && fs::is_directory(path) )
     return false;
   return true;
 }
 
 bool CanCheckCondition(u8 condition,
-		       const fs::path &x,
+		       const fs::path &path,
 		       size_t lenCurDir = 0) {
   try {
-    CheckCondition(condition,
-		   x,
-		   lenCurDir);
+    CheckCondition(condition, path);
   } catch(...) {
     return false;
   }
   return true;
 }
 
-
+//get extension of file
 std::string GetExtension(std::string &filename) {
   auto &res = *new std::string;
   for(auto it = filename.begin(); it != filename.end(); ++it) {
@@ -60,12 +60,12 @@ std::string GetExtension(std::string &filename) {
   return res;
 }
 
-void HandlePath(const fs::path &path,
-		u8 condition = 0b0,
-		size_t lenCurDir = 0) {
+//print file
+void DoHandlePath(fs::path &path,
+		  u8 condition = 0b0) {
+  path = path.filename();
   auto &pathStr = *new std::string(path.c_str());
-  pathStr = pathStr.substr(lenCurDir);
-  if(!CheckCondition(condition, path, lenCurDir)) {
+  if(!CheckCondition(condition, path)) {
     return;
   }
   if(fs::is_directory(path)) {
@@ -77,55 +77,103 @@ void HandlePath(const fs::path &path,
   }
   std::cout << pathStr << std::endl << "\033[0m";
 }
+void HandlePath(fs::path &dir,
+		u8 condition = DEFAULT_CONDITION) {
+  try {
+    DoHandlePath(dir, condition);
+  } catch(...) {
+    ReportError(*new std::string(boost::str(boost::format("Unexisting directory: %1%\n") % *new std::string(dir.c_str()) )), false);
+  }
+}
 
+//return condition
 std::pair<bool, Condition>
 HandleOption(std::string &option) {
   if(!option.compare("a") || !option.compare("-all"))
     return {false, STD};
   if(!option.compare("d") || !option.compare("-dirs") || !option.compare("-directories"))
     return {true, DIR};
-  throw boost::str(boost::format("Unknown option: %1%") % option);
+  throw boost::str(boost::format("Unknown option: \"%1%\"") % option);
 }
 
-void DoHandlePath(const fs::path &iterator,
-		  u8 condition = 0b0,
-		  size_t lenCurDir = 0) {
-  try {
-    HandlePath(iterator, condition, lenCurDir);
-  } catch(...) {
-    ReportError(*new std::string(boost::str(boost::format("Unexisting directory: %1%\n") % *new std::string(iterator.c_str()) )), false);
+void HandleProvided(fs::path const &path,
+		    std::vector<size_t> &lengths,
+		    u8 condition = DEFAULT_CONDITION,
+		    bool reversed = false,
+		    bool recursed = false) {
+  std::vector<fs::path> tmp;
+  
+  //handle recursed
+  if(!recursed) {
+    for(auto it: fs::directory_iterator(path)) {
+      tmp.push_back(it.path());
+    }
+  } else {
+    for(auto it: fs::recursive_directory_iterator(path)) {
+      tmp.push_back(it.path());
+    }
+  }
+
+  //handle reversed
+  if(reversed) {
+    std::reverse(tmp.begin(), tmp.end());
+  }
+  
+  {
+    //exit(69);
+    auto tmp_it = tmp.begin();
+    for( ;
+	 tmp_it != tmp.end();
+	 ++tmp_it ) {
+      HandlePath(*tmp_it, condition);
+    }
   }
 }
 
 int main(int argc, char **argv) {
+  //fill args
   auto &args = *new std::vector<std::string>();
   for(int i = 1; i < argc; ++i) {
     args.push_back(argv[i]);
   }
-  u8 condition  = STD;
+
+  //provided directories
+  auto &provided  = *new std::vector<fs::path const>;
+  //lengths of directories prefixes (like "./" in "./build.sh")
+  auto &lengths = *new std::vector<size_t>;
+  
+  u8 condition  = DEFAULT_CONDITION;
   bool reversed = false;
   bool recursed = false;
+  //for every arg
   for(int i = 0; i < args.size(); ++i) {
+    //current arg
     auto &src = *new std::string(args[i]);
     {
-      auto &tmp = *new std::pair<bool, Condition>;
+      //-r option check
       if(!src.compare("-r") ||
 	 !src.compare("--reversed")) {
 	reversed = true;
 	continue;
       }
+      //-R option check
       if(!src.compare("-R") ||
+	 !src.compare("--recurse") ||
 	 !src.compare("--recursed") ||
 	 !src.compare("--recursive")) {
 	recursed = true;
 	continue;
       }
+      //other options check
       if(src[0] == '-') {
+	//future result of function
+	auto &tmp = *new std::pair<bool, Condition>;
 	try {
 	  tmp = HandleOption(*new std::string(src.substr(1)));
 	} catch(std::string &e) {
 	  ReportError(e, false);
 	}
+	//if has to add condition to condition
 	if(tmp.first) {
 	  condition |= tmp.second;
 	} else {
@@ -135,25 +183,21 @@ int main(int argc, char **argv) {
       }
     }
 
-    const size_t lenCurDir = src.length()+1 - (src.back() == '/');
-    auto &res = *new std::vector<const fs::path>;
-    try {
-      if(!recursed)
-	for(const auto &entry: fs::directory_iterator(src))
-	  res.push_back(entry.path());
-      else
-	for(const auto &entry: fs::recursive_directory_iterator(src))
-	  res.push_back(entry.path());
-    } catch(...) {
-      ReportError(*new std::string(boost::str(boost::format("No such file or directory: %1%") % src)), false);
-    }
-    if(!reversed) {
-      for(auto it = res.begin(); it != res.end(); ++it) {
-	DoHandlePath(*it, condition, lenCurDir);
-      }
-    }
-    for(auto it = res.rbegin(); it != res.rend(); ++it) {
-      DoHandlePath(*it, condition, lenCurDir);
-    }
+    provided.push_back(fs::u8path(src));
   }
+
+  if(!provided.size()) {
+    provided.push_back(fs::u8path("./"));
+  }
+  
+  for(auto it: provided) {
+    // void HandleProvided(std::vector<fs::path const> &provided,
+    // 			std::vector<size_t> &lengths,
+    // 			u8 condition = DEFAULT_CONDITION,
+    // 			bool reversed = false,
+    // 			bool recursed = false);
+    HandleProvided(it, lengths, reversed, recursed);
+  }
+  
+  return 0;
 }
